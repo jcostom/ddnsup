237d94b6430e33a5837a46a510243ed0949e4cdb
Jason Costomiris <jcostom@jasons.org>
2023-11-27 10:44:07 -0500

    remove -dev version tag for merge to main

--- ddnsup.py
+++ ddnsup.py
@@ -65,7 +65,7 @@ SITENAME = os.getenv('SITENAME', 'mysite')
 
 
 # --- Globals ---
-VER = '0.9-dev'
+VER = '0.9'
 USER_AGENT = f"ddnsup.py/{VER}"
 IPCACHE = "/config/ip.cache.txt"
 HTTP_DATE_STRING = '%a, %d %b %Y %H:%M:%S GMT'
